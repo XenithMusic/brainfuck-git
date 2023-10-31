87ec3cba1c48315e5981e26db95e461e1d38b5d3
Landon <74lcheung@gmail.com>
2023-10-30 21:53:17 -0400

    copy of int.py

--- int.py
+++ int.py
@@ -1,20 +1,3 @@
-e=exec
-v=eval
-s=str
-z=lambda a:e("r"+s(r)+a)
-k=[];d=input("f:");r=0;c=0;o=""
-while c<(len(d)):
- i=d[c]
- if not "r"+s(r) in locals(): z("=0")
- if i in["+","-"]:z(i+"=1")
- if i=="<":r -= 1
- if i==">":r += 1
- if i==".":e("o+='"+chr(v("r"+s(r)))+"'");
- if i==",":z("=ord('"+input(">")[0]+"')")
- if i=="[":k=k+[c]
- if i=="]":
-  x=v("r"+s(r))!=0
-  if x:c=k[-1]
-  if not x:k=k[:-1]
- c+=1
-print(o)
+z=a=x=0
+c,m=input(),[0]*999
+while z<len(c):exec("a+=1|a-=1|m[a]+=1|m[a]-=1|print(chr(m[a]))|m[a]=ord(input())|x=z|if m[a]:z=x".split("|")["><+-.,[]".find(c[z])]);z+=1
